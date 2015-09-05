969078f7adec6d11db01f9675a6ffcb3d87913b3
namelos <namelos@outlook.com>
2015-09-05 11:19:11 +0800

    set template engine

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ import marked from 'marked';
 import fs from 'fs';
 
 var app = Express();
+
+app.set('view engine', 'jade');
+app.set('views', './');
+
 app.get('/', (req, res) =>
   fs.readFile('./test.md', 'utf-8', (err, data) =>
-    res.send(marked(data))))
+    res.render('index', {content: marked(data)})))
 
 app.listen(3000)
